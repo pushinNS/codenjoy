ef275e9306af1de56da14f59be64ffd93a447029
igor petrov <igor_petrov@epam.com>
2017-10-27 09:14:03 +0400

    postgresql

--- CodingDojo/builder/start-server.bat
+++ CodingDojo/builder/start-server.bat
@@ -1,1 +1,3 @@
-call mvn -DMAVEN_OPTS=-Xmx1024m -Dmaven.test.skip=true clean jetty:run-war
+SET SPRING_PROFILES_ACTIVE=postgre
+SET MAVEN_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=8000 -Xmx1024m -Dmaven.test.skip=true
+call mvn clean jetty:run-war
